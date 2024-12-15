916e1653a50154fa215aaebf3d84b104b827031f
suzan <sevcihansuzanbagci@gmail.com>
2024-12-15 09:59:16 +0300

    new-feat

---
+++ ieee-web/Controllers/AboutController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ieee_web.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        public ActionResult History()
+        {
+            return View();
+        }
+
+        public ActionResult VisionMission()
+        {
+            return View();
+        }
+    }
+}
---
+++ ieee-web/Controllers/CommiteesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ieee_web.Controllers
+{
+    public class CommiteesController : Controller
+    {
+        // GET: Commitees
+        public ActionResult Comsoc()
+        {
+            return View();
+        }
+
+        public ActionResult cs()
+        {
+            return View();
+        }
+    }
+}
